c98448f57fb9c4d48126cc9c61e78e4a33baf9b9
Woogie-94 <dmsdmr9411@gmail.com>
2023-10-13 22:34:36 +0900

    feat: Toast UI를 사용해 에러 처리

--- src/pages/main/presenter/Quiz.presenter.ts
+++ src/pages/main/presenter/Quiz.presenter.ts
@@ -3,11 +3,14 @@ import { useNavigate } from "react-router-dom";
 import { useQuizInteractorContext } from "../interactor/Quiz.interactor";
 import { PATH_QUESTION } from "../../../constants/path";
 import { QUESTION_RESPONCE_CODE__SUCCESS } from "../../../constants/question";
+import useToast from "../../../hooks/useToast";
 
 export type QuizPresenterResult = ReturnType<typeof useQuizPresenter>;
 export const useQuizPresenter = () => {
   const { fetchQuestion } = useQuizInteractorContext();
   const navigation = useNavigate();
+
+  const { show } = useToast();
 
   const onStartClick = () => {
     // Note(eunwook): openAPI가 요청이 실패해도 200 status로 내려옵니다. 대신 response_code로 에러를 판별하도록 가이드 되어 있습니다.
@@ -15,7 +18,7 @@ export const useQuizPresenter = () => {
       if (data?.responseCode === QUESTION_RESPONCE_CODE__SUCCESS) {
         navigation(`${PATH_QUESTION}?step=1`, { state: { questions: data?.questions } });
       } else {
-        // toast
+        show({ message: `요청이 실패했습니다. (code-${data?.responseCode})` });
       }
     });
   };
