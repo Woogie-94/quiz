11f7edcee6e0d20107701d7e0f8021c78e64b78b
Woogie-94 <dmsdmr9411@gmail.com>
2023-10-13 22:33:35 +0900

    feat: Toast UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import GlobalStyle from "./styles/globalStyle";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastProvider } from "./contexts/toastContext";
+import Toast from "./components/Toast";
 
 export const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ToastProvider>
+        <RouterProvider router={router} />
+        <Toast />
+      </ToastProvider>
       <GlobalStyle />
     </QueryClientProvider>
   );
