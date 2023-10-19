d50a7c5facb5d3f1436fdea746375be6e2adcaf8
Woogie-94 <dmsdmr9411@gmail.com>
2023-10-19 16:28:02 +0900

    feat: 다시 도전하기 버튼 추가

--- src/pages/result/components/Page.tsx
+++ src/pages/result/components/Page.tsx
@@ -4,6 +4,9 @@ import Chart from "./Chart";
 import IncorrectAnswerNote from "./IncorrectAnswerNote";
 import Score from "./Score";
 import TotalTime from "./TotalTime";
+import Button from "../../../components/Button";
+import { Link } from "react-router-dom";
+import { PATH_ROOT } from "../../../constants/path";
 
 const Page = () => {
   return (
@@ -13,6 +16,9 @@ const Page = () => {
         <Chart />
         <Score />
         <TotalTime />
+        <ButtonWrapper to={PATH_ROOT}>
+          <Button label="다시 도전하기" />
+        </ButtonWrapper>
         <IncorrectAnswerNote />
       </>
     </AppLayout>
@@ -25,4 +31,9 @@ const Title = styled.h2`
   font-weight: bold;
   font-size: 24px;
   color: #424242;
+`;
+
+const ButtonWrapper = styled(Link)`
+  display: block;
+  padding-top: 16px;
 `;
